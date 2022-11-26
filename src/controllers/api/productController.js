df82192eb6f3b18df84e3c16b00537abedb59017
knnv-ar <knvv.ar@gmail.com>
2022-11-25 23:07:36 -0300

    feat: create controller and route > productController.js and product.js

--- src/controllers/api/productController.js
+++ src/controllers/api/productController.js
@@ -1,6 +1,30 @@
+const db = require('../../database/models');
+
 const controller = {
-    index: (req, res) => {
-        // Crear controladores para retornar todos los productos
+    // Crear controladores para retornar todos los productos
+    index: async (req, res) => {
+        try {
+            const products = await db.Product.findAll();
+            return res.json({
+                status: 200,
+                data: products
+            });
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    },
+
+    create: async (req, res) => {
+        try {
+            const { name, description, price, product_categories_id } = req.body;
+            const productCreated = await db.Product.create({ name, description, price, product_categories_id });
+            return res.status(201).json({
+                status: 201,
+                message: 'Product created'
+            });
+        } catch (error) {
+            return res.status(500).json(error);
+        }
     }
 }
 
