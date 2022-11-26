b8d5d6d2d830a327121148ad6cfde3e3dff73837
Andrey Bejarano <andreybejarano@gmail.com>
2022-11-25 22:06:11 -0300

    feat: create endpoint to get and create roles

---
+++ src/controllers/api/categoryProductController.js
@@ -0,0 +1,7 @@
+const controller = {
+    index: (req, res) => {
+        // Crear controladores para retornar todos los productos
+    }
+}
+
+module.exports = controller;
---
+++ src/routers/api/categoryProduct.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const productController = require('../../controllers/api/categoryProductController');
+
+const router = express.Router();
+
+// Implementar logica para retornar todos las categorias de productos
+// router.???('/', productController.index);
+
+module.exports = router;
